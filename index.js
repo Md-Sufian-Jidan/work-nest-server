06aee9d3958c86d5f29696ffdec4ea4e24534f48
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-04-21 19:29:51 +0600

    get the work sheet data api added

--- index.js
+++ index.js
@@ -122,6 +122,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/work-sheet/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await workSheetCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.get('/employees-list', async (req, res) => {
       const result = userCollection.find().toArray();
       res.send(result);
